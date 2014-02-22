7effdd8253fbcbac9c64bb649ea9ddb6e9bc86c7
Platos <Grayworgen@gmail.com>
2014-02-23 01:56:51 +0400

    Select signal & more readable improvements
    Many changes in pltSignal file. Select signal and select synch leap is
    tow different stage. Other improvements relate to the visual component
    of the code.

--- bin/SYNCHandSMOTH.m
+++ bin/SYNCHandSMOTH.m
@@ -1,19 +1,16 @@
-function [Isn Usn Ic Ics]=SYNCHandSMOTH(I,U,NSmoothU,NSmoothI,NSmoothIc,X)
+function [Isn, Usn, Ic, Ics]=SYNCHandSMOTH(I,U,NSmoothU,NSmoothI,NSmoothIc,X)
     Is=fastsmooth(I,NSmoothI);
     Us=fastsmooth(U,NSmoothU);
     global IFreq Tr UFreq
+    
     % Find synchronization point
-%     [value nI]=max(Is(X(3):X(4)));
-%     nI=nI+1+X(3);
-%     [value nU]=max(fastsmooth(diff(Us(X(5):X(6))),NSmoothIc));
-%     nU=nU+1+X(5);
     if max(Is)<1e3
         dUs_=fastsmooth(diff(Us(X(5):X(6))),NSmoothIc);
         Is_=Is(X(3):X(4));
         a1=Is_/max(Is_);
         a2=dUs_/max(dUs_);
         asd=xcorr(a1,a2);
-        [max_asd,Nm_asd]=max(asd);
+        [~,Nm_asd]=max(asd);
         iI_=X(3)-X(5)+Nm_asd-round(length(asd)/2);
         if iI_>0
         Isn=Is(iI_:end);
@@ -30,8 +27,8 @@ function [Isn Usn Ic Ics]=SYNCHandSMOTH(I,U,NSmoothU,NSmoothI,NSmoothIc,X)
         end
     else
         % Find synchronization point
-        [value nI]=max(Is);
-        [value nU]=max(Us);
+        [~, nI]=max(Is);
+        [~, nU]=max(Us);
         % Synchronization
         iI=1+abs(nU-nI)/2+(nI-nU)/2;
         iU=1+abs(nU-nI)/2+(nU-nI)/2;
@@ -53,12 +50,12 @@ function [Isn Usn Ic Ics]=SYNCHandSMOTH(I,U,NSmoothU,NSmoothI,NSmoothIc,X)
 % %     dNc=IFreq/NSmoothU*Tr;
     
     if max(Isn)<1e3
-        [ValI NmaxI]=max(Isn(X(3)-iI_*aI:X(4)-iI_*aI));
+        [~, NmaxI]=max(Isn(X(3)-iI_*aI:X(4)-iI_*aI));
         NmaxI=NmaxI+X(3)-iI_*aI-1;
         dNc=IFreq/NSmoothU*Tr*10;
         C=Isn(NmaxI-dNc:NmaxI+dNc)./diff(Usn(NmaxI-dNc:NmaxI+dNc+1))/UFreq;
         asd=ne(C,Inf);
-        N_pos=find(asd);
+        N_pos= asd;
         C=C(N_pos);
 %         for k=1:length(C)
 %             if C(k)==Inf
--- bin/pltSignal.m
+++ bin/pltSignal.m
@@ -1,36 +1,50 @@
-function [X Ks]=pltSignal(UFreq,IFreq,I,U)
-    Is=fastsmooth(I,IFreq/50);
-    Us=fastsmooth(U,UFreq/50);
-%    SelectFig = figure;
-Ks=1;
-if length(Us)>=3e5
-    Ks=2;
-    if length(Us)>=5e5
-        Ks=5;
-        if length(Us)>=2e6
-            Ks=10;
-        end
-    end
-end
-    plot(Is(1:Ks:end)/ max(Is), 'b');
-    hold on
-    plot(Us(1:Ks:end)/ max(Us), 'r');
-    [x1, y1] = ginput(2);
-    legend('');
-    hold off
-%    close(SelectFig);
-    tic
-    X=[round(min(x1)) round(max(x1)) 0 0 0 0]*Ks;
-%    SelectFig = figure;
-    plot(Is(X(1):Ks:round(X(2)/Ks))/ max(Is), 'b');
-    [x2, y2] = ginput(2);
-%    close(SelectFig);
-    tic
-%    SelectFig = figure;
-    plot(Us(X(1):Ks:round(X(2)/Ks))/ max(Us), 'r');
-    [x3, y3] = ginput(2);
-%    close(SelectFig);
-    tic
-    
-    X=round([min(x1) max(x1) min(x2) max(x2) min(x3) max(x3)]*Ks);
+function [X, Ks]=pltSignal(UFreq,IFreq,I,U)
+Is=fastsmooth(I,IFreq/50);% Averaging with respect the fundamental frequency 
+Us=fastsmooth(U,UFreq/50);% interference (industrial frequency of 50 Hz)
+
+Ks=round(length(Us)/1000);% Drow grafic in 1000 points ratio
+
+%~~~~~~~Select signal length~~~~~~~
+plot(Is(1:Ks:end)/ max(Is), 'b');
+hold on
+plot(Us(1:Ks:end)/ max(Us), 'r');
+[x1, ~] = ginput(2);
+x1=round(x1*Ks);
+legend('');
+hold off
+tic
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%~~Select sinchronization interval~~
+plot(Is(1:Ks:end)/ max(Is), 'b');
+hold on
+plot(Us(1:Ks:end)/ max(Us), 'r');
+hold off
+[xs, ~] = ginput(2);
+xs=round(xs*Ks);
+tic
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%~~~~~Select current sinch leap~~~~~
+L=length(Is(min(xs):max(xs)));
+tKs=round(L/1000);
+plot(Is(min(xs):tKs:max(xs))/ max(Is(min(xs):tKs:max(xs))), 'b');
+[x2, ~] = ginput(2);
+x2=round(x2*tKs)+min(xs);
+tic
+%figure, plot(Is(min(x2):tKs:max(x2))) test
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%~~~~~Select voltage sinch leap~~~~~
+L=length(Us(min(xs):max(xs)));
+tKs=round(L/1000);
+plot(Us(min(xs):tKs:max(xs))/ max(Us(min(xs):max(xs))), 'r');
+[x3, ~] = ginput(2);
+x3=round(x3*tKs)+min(xs);
+tic
+%figure, plot(Us(min(x3):tKs:max(x3))) test
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+X = [min(x1) max(x1) min(x2) max(x2) min(x3) max(x3)];
+%~~~~~~~~Signal~~~~~~~~~~Current~~~~~~~~~Voltage~~~~~~
 end
